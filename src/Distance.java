0862550cf5248be4115ea3d53ea9742941371268
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-25 14:04:23 +0530

    ADDED : code for calculating eucledian distance

---
+++ src/Distance.java
@@ -0,0 +1,8 @@
+
+public class Distance {
+    public static void main(String[] args) {
+        int x = 2, y = 3;
+        float distance = (float) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        System.out.println("Eucledian Distance = " + distance);
+    }
+}
